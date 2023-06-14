535fc14b683f3cf3ed414eb765ece5ede49f4c97
RocioAlday <roo.alday@gmail.com>
2023-06-14 12:24:20 -0300

    template-react-vite

---
+++ client/frontend-fullprism/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default App;
